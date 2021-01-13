683ae9c24c32d3bc2c13f9333ef106d35702c68b
Jennifer LaRue <jennifer@photoshelter.com>
2021-01-12 21:54:46 -0500

    updatedScript

--- js/script.js
+++ js/script.js
@@ -19,63 +19,32 @@ const quotes = [
   },
   {
     quote: "Insanity: doing the same thing over and over again and expecting different results.",
-    author:  "Albert Einstein" // CHECK
+    author:  "Albert Einstein" 
   },
   {
     quote: "The greatest wealth is to live content with little." ,
-    author:  "Plato" // CHECK
+    author:  "Plato" 
   },
   {
     quote: "The future belongs to those who prepare for it today.",
     author:  "Malcolm X",
-    year: 1962 // CHECK
+    year: 1962 
   },
   {
     quote: "The successful warrior is the average man, with laser-like focus.",
     author:  "Bruce Lee",
-    citation: "Motivational Phrases" // CHECK
+    citation: "Motivational Phrases" 
   },
   {
     quote: "Well done is better than well said.",
-    author:  "Benjamin Franklin" // CHECK
+    author:  "Benjamin Franklin"
   }
 ]
 
 
-/**
- * Using a for loop to name each object's key value pairs within the array 
- */
-// for(let i = 0; i < quoteArray.length; i++) {
-//   let famousQuote = quoteArray[i].quote;
-//   let famousAuthor = quoteArray[i].author;
-//   let year = quoteArray[i].year;
-//   let citation = quoteArray[i].citation;
-//   const quoteObject = {};
-
-//     if (Object.keys(quoteArray[i]).includes('year')) {
-//       quoteObject = {
-//         quote: famousQuote,
-//         author: famousAuthor,
-//         year: year
-//       }
-//     } else if(Object.keys(quoteArray[i]).includes('citation')) {
-//       quoteObject = {
-//         quote: famousQuote,
-//         author: famousAuthor,
-//         citation: citation
-//       }
-//     } else {
-//       quoteObject = {
-//         quote: famousQuote,
-//         author: famousAuthor
-//       }
-//     }
-// };
-
-
-// /***
-//  * `getRandomQuote` function
-// ***/
+/***
+  * `getRandomQuote` function
+***/
 //should randomly select quote from the quotes array and return it
 function getRandomQuote() {
   const randomNumber = Math.floor(Math.random() * quotes.length);
